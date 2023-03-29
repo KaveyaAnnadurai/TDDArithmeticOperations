ffe6dc6903d78d9a85bdbb240c4fc8bd5d2a77ed
KaveyaAnnadurai <kaveya.a@m2pfintech.com>
2023-03-29 12:20:45 +0530

    Initial commit done.

---
+++ src/test/java/com/m2p/ArithmeticOperationsTest.java
@@ -0,0 +1,19 @@
+package com.m2p;
+
+import org.junit.jupiter.api.Test;
+
+public class ArithmeticOperationsTest {
+
+    @Test
+    void getFourWhenThreeAddedToOne(){
+        //Arrange
+        ArithmeticOperations arithmeticObject = new ArithmeticOperations();
+//        int expectedValue = 4;
+//
+//        //Act
+//        int actualValue = arithmeticObject.add(3,1);
+//
+//        //Assert
+//        assertEquals(expectedValue,actualValue);
+    }
+}
