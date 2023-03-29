8632b246b8b5cdf4d4bf71bc879cac1cf7873e0d
KaveyaAnnadurai <kaveya.a@m2pfintech.com>
2023-03-29 12:34:21 +0530

    Add. Included testcase for Addition.

--- src/main/java/com/m2p/ArithmeticOperations.java
+++ src/main/java/com/m2p/ArithmeticOperations.java
@@ -2,4 +2,8 @@ package com.m2p;
 
 public class ArithmeticOperations {
 
+    int add(int firstInputNumber, int secondInputNumber){
+        return firstInputNumber+secondInputNumber;
+    }
+
 }
--- src/test/java/com/m2p/ArithmeticOperationsTest.java
+++ src/test/java/com/m2p/ArithmeticOperationsTest.java
@@ -2,18 +2,22 @@ package com.m2p;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ArithmeticOperationsTest {
 
     @Test
     void getFourWhenThreeAddedToOne(){
         //Arrange
         ArithmeticOperations arithmeticObject = new ArithmeticOperations();
-//        int expectedValue = 4;
-//
-//        //Act
-//        int actualValue = arithmeticObject.add(3,1);
-//
-//        //Assert
-//        assertEquals(expectedValue,actualValue);
+        int expectedValue = 4;
+
+        //Act
+        int actualValue = arithmeticObject.add(3,1);
+
+        //Assert
+        assertEquals(expectedValue,actualValue);
     }
+
+
 }
